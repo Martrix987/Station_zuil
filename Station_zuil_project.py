f0258040f048aa98f20ce814afb63dfea43eaa75
Martrix987 <marnix2hendriks@gmail.com>
2022-10-13 14:48:01 +0200

    init

--- Station_zuil_project.py
+++ Station_zuil_project.py
@@ -7,8 +7,8 @@ def klacht():
     if naam == (''):
         print('U heeft er voor gekozen om uw opinie anoniem in te dienen.')
         opinie = input('U kan hier uw opinie anoniem indienen, (de opinie mag uit maximaal 140 karakters bestaan): ')
-        if len(opinie) < 140:
-            print('U heeft te veel karakters gebruikt, probeer het opnieuw')
+        if len(opinie) < 140 or opinie == '/' or naam == '/':
+            print('U heeft te veel karakters gebruikt of een / gebruikt, probeer het opnieuw')
             break
         else:
             return naam, opinie
@@ -16,8 +16,8 @@ def klacht():
     else:
         print('Goedendag,', naam, 'U voert uw opinie in onder uw eigen naam.')
         opinie = input('U kan hier uw opinie openbaar indienen, (de opinie mag uit maximaal 140 karakters bestaan): ')
-        if len(opinie) < 140:
-            print('U heeft te veel karakters gebruikt, probeer het opnieuw')
+        if len(opinie) < 140 or opinie == '/' or naam == '/':
+            print('U heeft te veel karakters gebruikt of een / gebruikt, probeer het opnieuw')
             break
         else:
             return naam, opinie
