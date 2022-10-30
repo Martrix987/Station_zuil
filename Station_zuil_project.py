1d9c016d425bf63f77ba18d06cbf9e6f630f85da
Marnix Hendriks <marnix2hendriks@gmail.com>
2022-10-30 22:29:05 +0100

    init

--- Station_zuil_project.py
+++ Station_zuil_project.py
@@ -5,28 +5,26 @@ import random
 def opinie():
     naam = input('Voer hier uw naam in: ')
     #verander het woord klacht want het mag ook postief --> opinie
-    if naam == (''):
+    if naam == ('') or naam == (' '):
         print('U heeft er voor gekozen om uw opinie anoniem in te dienen.')
-        opinie = input('U kan hier uw opinie anoniem indienen, (de opinie mag uit maximaal 140 karakters bestaan): ')
-        if len(opinie) < 140 or opinie == '/' or naam == '/':
+        opinie = input('u kan hier uw opinie anoniem indienen, (de opinie mag uit maximaal 140 karakters bestaan en geen: /): ')
+        if len(opinie) > 140 or opinie.find('/') or naam.find('/'):
             print('U heeft te veel karakters gebruikt of een / gebruikt, probeer het opnieuw')
-            break
         else:
-            return naam, opinie
+            return False, opinie
             
     else:
         print('Goedendag,', naam, 'U voert uw opinie in onder uw eigen naam.')
         opinie = input('U kan hier uw opinie openbaar indienen, (de opinie mag uit maximaal 140 karakters bestaan): ')
-        if len(opinie) < 140 or opinie == '/' or naam == '/':
+        if len(opinie) > 140 or opinie == '/' or naam == '/':
             print('U heeft te veel karakters gebruikt of een / gebruikt, probeer het opnieuw')
-            break
         else:
             return naam, opinie
 
-opinie()
+print(opinie())
 
 
-
+#vraag voor docent hoe fliter ik op /
 
 
 
