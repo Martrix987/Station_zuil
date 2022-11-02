51e4a5afbb268bf4799b3949236529ab6c2cee78
Marnix Hendriks <marnix2hendriks@gmail.com>
2022-11-02 10:46:33 +0100

    init

--- Station_zuil_project_check.py
+++ Station_zuil_project_check.py
@@ -1,5 +1,8 @@
+from xmlrpc.client import boolean
 import psycopg2.extras
 import psycopg2
+import datetime
+
 
 
 connection_string = "host='localhost' dbname='station_zuil_database' user='postgres' password='128256'"
@@ -15,3 +18,58 @@ query = """SELECT     *
 cursor.execute(query)
 print(cursor.fetchall())
 conn.close()
+
+
+
+keuring = boolean(input('\nControleer of de tekst hierboven fatsoenlijk is, indien de tekst fatsoenlijk is schrijf True maar als de tekst niet fatsoenlijk is schrijf False'))
+if keuring == True:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+naam_moderator = input('Voer hier de naam van de moderator in: ')
+email_moderator = input('Voer hier het email van de moderator in: ')
+datum_tijd = datetime.datetime.now()
+
+connection_string = "host='localhost' dbname='station_zuil_database' user='postgres' password='128256'"
+conn = psycopg2.connect(connection_string) 
+cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+cursor.execute("INSERT INTO moderatie (datumtijd_beoordeling, naam, bericht, station) VALUES (%s, %s, %s, %s)", (datum_tijd, goedkeuring, naam_moderator, email_moderator))
+
+
+conn.commit()
+cursor.close()
+conn.close()
+
+
+
+
+
+
+
+
+
+
+
