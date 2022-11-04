a07af629cbaba8b2117009be157aecfa8d7e8b7e
Martrix987 <marnix2hendriks@gmail.com>
2022-11-04 13:53:58 +0100

    init

---
+++ 123.py
@@ -0,0 +1,4 @@
+bericht = ("[hello world]")
+striped_bericht = bericht.strip("[]")
+print(bericht)
+print(striped_bericht)
--- Station_zuil_project_check.py
+++ Station_zuil_project_check.py
@@ -18,7 +18,44 @@ bericht = cursor.fetchall()
 conn.close()
 
 
+print(type(bericht))
 print(bericht)
+bericht
+
+'''
+def convert(ber):
+ 
+    # initialize an empty string
+    str1 = ""
+ 
+    # traverse in the string
+    for ele in ber:
+        str1 += ele
+ 
+    # return string
+    return str1
+ 
+ 
+# Driver code
+con_bericht = convert(bericht)
+print(type(con_bericht))
+print(con_bericht)
+print(con_bericht.strip("[]"))
+'''
+'''
+def convertTuple(tup):
+        # initialize an empty string
+    bericht = ''
+    for item in tup:
+        bericht = bericht + item
+    return bericht
+str_bericht = convertTuple(bericht)
+print(str_bericht)
+print(str_bericht.strip("[]"))
+'''
+
+
+
 
 
 test_keuring = int(input('\nControleer of de tekst hierboven fatsoenlijk is, indien de tekst fatsoenlijk is type 1 maar als de tekst niet fatsoenlijk is typ 2 \n: '))
@@ -35,12 +72,18 @@ else:
     keuring = 0
 
 
+print(datum_tijd)
+print(keuring)
+print(mod_id)
+print(bericht)
+
+
 connection_string = "host='localhost' dbname='station_zuil_database' user='postgres' password='128256'"
 conn = psycopg2.connect(connection_string) 
 cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-cursor.execute("update bericht set datumtijd_beoordeling = '2022-11-02 010:35:23', goedkeuring = 1, mod_id = 1 WHERE bericht = 'hello world1'")
-#cursor.execute("update INTO bericht (datumtijd_beoordeling, goedkeuring, mod_id, station) VALUES (%s, %s, %s)", (datum_tijd, keuring, mod_id))
+cursor.execute("update bericht set datumtijd_beoordeling = %s, goedkeuring = %s, mod_id = %s WHERE bericht = '%s'", (datum_tijd, keuring, mod_id, bericht))
+#cursor.execute("update bericht set datumtijd_beoordeling = '2022-11-02 010:35:23', goedkeuring = 1, mod_id = 1 WHERE bericht = (%s)", (datum_tijd, keuring, mod_id, bericht))
 
 conn.commit()
 cursor.close()
