761205c4355411767cca32bed440bd48a65ff967
Marnix Hendriks <marnix2hendriks@gmail.com>
2022-11-07 10:21:30 +0100

    init

--- 123.py
+++ 123.py
@@ -1,14 +1,18 @@
 from tkinter import *
-
+from functools import partial
 
 
 root = Tk()
+root.title("NS_station's_bord")
 
 achtergrond = PhotoImage(file='station_zuil_achtergrond.png')
 label = Label(
+image = achtergrond,
 master=root, 
-text='Hello World',
-image = achtergrond
+text='Hello World', 
+height=720,
+width=1080,
+
 
 
 )
