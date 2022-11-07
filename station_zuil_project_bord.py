1a7ad634d74e5ecb802f370349e507d7f5d45cbc
Martrix987 <marnix2hendriks@gmail.com>
2022-11-07 16:35:28 +0100

    init

--- station_zuil_project_bord.py
+++ station_zuil_project_bord.py
@@ -19,33 +19,8 @@ def click_amsterdam():
 
     stad = 'Amsterdam'
     weerbericht(stad)
-
-    connection_string = "host='localhost' dbname='station_zuil_database' user='postgres' password='128256'"
-    conn = psycopg2.connect(connection_string) 
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
-    query = """SELECT bericht, naam
-                FROM bericht
-                ORDER BY datumtijd_bericht DESC
-                LIMIT 5;
-            ;"""
-    cursor.execute(query)
-    non_con_bericht = cursor.fetchall()
-    conn.close()
-    print(non_con_bericht)
-
-    berichten = Label(master=root, text='con_bericht', foreground='blue', font=('Helvetica', 16), bg='yellow',)
-    berichten.pack()
-    
-    
-    
-    
-
-
-    
-
-
-
-
+    berichten()
+ 
 
 
 def click_leiden():
@@ -56,25 +31,8 @@ def click_leiden():
 
     stad = 'Leiden'
     weerbericht(stad)
+    berichten()
 
-    connection_string = "host='localhost' dbname='station_zuil_database' user='postgres' password='128256'"
-    conn = psycopg2.connect(connection_string) 
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
-    query = """SELECT bericht, naam
-                FROM bericht
-                ORDER BY datumtijd_bericht DESC
-                LIMIT 5;
-            ;"""
-    cursor.execute(query)
-    non_con_bericht = cursor.fetchall()
-    conn.close()
-    print(non_con_bericht)
-
-    berichten = Label(master=root, text='con_bericht', foreground='blue', font=('Helvetica', 16), bg='yellow',)
-    berichten.pack()
-
-    stad = 'Leiden'
-    return stad
 
 
 def click_utrecht():
@@ -85,26 +43,7 @@ def click_utrecht():
 
     stad = 'Utrecht'
     weerbericht(stad)
-    
-
-    connection_string = "host='localhost' dbname='station_zuil_database' user='postgres' password='128256'"
-    conn = psycopg2.connect(connection_string) 
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
-    query = """SELECT bericht, naam
-                FROM bericht
-                ORDER BY datumtijd_bericht DESC
-                LIMIT 5;
-            ;"""
-    cursor.execute(query)
-    non_con_bericht = cursor.fetchall()
-    conn.close()
-    print(non_con_bericht)
-
-    berichten = Label(master=root, text='con_bericht', foreground='blue', font=('Helvetica', 16), bg='yellow',)
-    berichten.pack()
-
-    stad = 'Utrecht'
-    return stad
+    berichten()
 
 
 
@@ -113,7 +52,6 @@ root = Tk()
 root.configure(bg='yellow')
 root.geometry("1080x720")
 root.resizable(False, False)
-
 
 kiezen = Label(
 bg='yellow', master=root, text='Choose you current location', foreground='blue', font=('Helvetica', 16),)
@@ -139,6 +77,27 @@ amsterdam.place(x=545, y=360)
 leiden = Button(master=root, text='Leiden', command=click_leiden, foreground='blue', bg='yellow',)
 leiden.pack(pady=10)
 leiden.place(x=666, y=360)
+def berichten():
+    connection_string = "host='localhost' dbname='station_zuil_database' user='postgres' password='128256'"
+    conn = psycopg2.connect(connection_string) 
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+    query = """SELECT bericht, naam
+                FROM bericht
+                ORDER BY datumtijd_bericht DESC
+                LIMIT 5;
+            ;"""
+    cursor.execute(query)
+    non_con_bericht = cursor.fetchall()
+    conn.close()
+    print(non_con_bericht)
+
+    berichten = Label(master=root, text='con_bericht', foreground='blue', font=('Helvetica', 16), bg='yellow',)
+    berichten.pack()
+
+
+
+
+
 
 def weerbericht(stad):
     tempratuur_txt = Label(master=root, text='Tempratuur in graden celcius:', foreground='blue', font=('Arial', 20), bg='yellow', )
@@ -172,17 +131,15 @@ def weerbericht(stad):
     Weersvoorspellinge = Label(master=root, text='Weersvoorspellinge:', foreground='blue', font=('Arial', 20), bg='yellow', )
     Weersvoorspellinge.pack()
     Weersvoorspellinge.place(x=735, y=0)    
-    temp_amst = Label(master=root, text=temp, foreground='blue', font=('Arial', 20), bg='yellow', )
-    temp_amst.pack()
-    temp_amst.place(x=1000, y=50)
-    gevoel_amst = Label(master=root, text=gevoels_temp, foreground='blue', font=('Arial', 20), bg='yellow',)
-    gevoel_amst.pack()
-    gevoel_amst.place(x=1000, y=100)
-    vochtig_amst = Label(master=root, text=luchtvochtigheid, foreground='blue', font=('Arial', 20), bg='yellow',)
-    vochtig_amst.pack()
-    vochtig_amst.place(x=1000, y=150)  
-
-
+    temp = Label(master=root, text=temp, foreground='blue', font=('Arial', 20), bg='yellow', )
+    temp.pack()
+    temp.place(x=1000, y=50)
+    gevoel = Label(master=root, text=gevoels_temp, foreground='blue', font=('Arial', 20), bg='yellow',)
+    gevoel.pack()
+    gevoel.place(x=1000, y=100)
+    vochtig = Label(master=root, text=luchtvochtigheid, foreground='blue', font=('Arial', 20), bg='yellow',)
+    vochtig.pack()
+    vochtig.place(x=1000, y=150)  
 
 
 root.mainloop()
